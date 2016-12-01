b8caddfcacaa98f0ba4f8c91efe907ac21b6dece
facert <zhangcr1992@163.com>
2016-12-01 12:33:32 +0800

    fix(replace url)

--- auto_reply_douban.py
+++ auto_reply_douban.py
@@ -6,10 +6,11 @@ import time
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36', 'Cookie': '****'}
 payloads = {'ck': 'n1iQ', 'rv_comment': 'up', 'start': '0', 'submit_btn': u'加上去'}
+url = '***'
 
 def main():
     while True:
-        res = requests.post('https://www.douban.com/group/topic/93461964/add_comment', data=payloads, headers=headers)
+        res = requests.post(url, data=payloads, headers=headers)
         print res
         time.sleep(60*60)
 
